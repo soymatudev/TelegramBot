9c4020bb6af35c7627cab2fe0cd7bfb84cd272aa
soymatudev <juanjesusgarciamaturana@gmail.com>
2025-07-27 07:40:42 -0600

    first commit

---
+++ Boty01.php
@@ -0,0 +1,18 @@
+<?php
+
+$token = "8024363859:AAE0AI1EXq7jGcrjeih170mPgEsd60Xg8vo";
+$apiURL = "https://api.telegram.org/bot$token/sendMessage";
+
+$update = json_decode(file_get_contents('php://input'), true);
+
+$chat_id = $update['message']['chat']['id'];
+$message = $update['message']['text'] ?? "";
+
+if ($message) {
+    $respuesta = "Hola, soy un bot de Telegram. ¿En qué puedo ayudarte?";
+    file_get_contents("$apiURL?chat_id=$chat_id&text=" . urlencode($respuesta));
+}
+
+// curl -X GET "https://api.telegram.org/bot8024363859:AAE0AI1EXq7jGcrjeih170mPgEsd60Xg8vo/setWebhook?url=https://127.0.0.1/bot/request.php"
+
+// curl --request GET --url "https://api.telegram.org/bot8024363859:AAE0AI1EXq7jGcrjeih170mPgEsd60Xg8vo/getWebhookInfo"
