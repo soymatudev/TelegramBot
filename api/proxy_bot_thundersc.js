4fb1bc39766d3ec1740340f0814c864f66632aea
soyMatuDev <juanjesusgarciamaturana@gmail.com>
2025-08-05 09:19:17 -0600

    19 commit

--- api/proxy_bot_thundersc.js
+++ api/proxy_bot_thundersc.js
@@ -1,4 +1,72 @@
-import Bridge from "./Bridge.js";
+
+class Bridge{
+  #usu; #com; #url; #param; #header; #ff;
+
+  constructor(uu, cc, url, pp = [''], hh = {'Content-Type': 'application/json'}){
+      this.#usu = uu;
+      this.#com = cc;
+      this.#url = url;
+      this.#param = pp;
+      this.#header = hh;
+      this.urlFormat();
+  }
+
+  async databriged(){
+      try {
+          const response = await fetch(this.#url, {
+              method: "POST",
+              headers: this.#header,
+              body: this.paramsFormat(),
+          });
+
+          if (response.status === 500) {
+              throw new Error("Error interno del servidor");
+          } else if (response.status === 400) {
+              console.log("Error en la solicitud:", data.error);
+          } else if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          //const contentType = response.headers.get("Content-Type");
+          return response;
+      } catch (error) {
+          console.log(error);
+      }
+  } 
+
+  paramsFormat(){
+      let param = JSON.stringify({
+          function: this.#ff,
+          args: this.#param,
+          uu: this.#usu,
+          cc: this.#com
+      });      
+
+      return param;
+  }
+
+  urlFormat(){
+      // Ejemplo: System.Inventario.Catalogos.Articulos.getArticulos
+      let url = this.#url.split(".");
+      this.#ff = url.pop();
+      this.#url = "http://nexthwd.pcz.com.mx:4480/thundersc/thundercloud/" + url.join("/") + ".php";
+  }
+
+  async downloadEvent(response) {
+      // Si el contenido es un archivo descargable, descárgalo directamente
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = response.headers
+      .get("Content-Disposition")
+      .split("filename=")[1]
+      .replace(/\"/g, "");
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+  }
+}
 
 export default async function handler(req, res) {
   try {
