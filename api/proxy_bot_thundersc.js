543ebbac1bf3bbce1b894f3377ce53e1910bbf55
soyMatuDev <juanjesusgarciamaturana@gmail.com>
2025-08-05 13:32:04 -0600

    31 commit

--- api/proxy_bot_thundersc.js
+++ api/proxy_bot_thundersc.js
@@ -89,7 +89,7 @@ export default async function handler(req, res) {
     console.log("Cuerpo limpio recibido:", cleanBody);
 
 
-    if (cleanBody.text == "Just") {
+    if (cleanBody.message.text == "Just") {
       console.log("Mensaje 'Just' recibido, ignorando...");
       return res.status(200).json({ ok: true });
     }
